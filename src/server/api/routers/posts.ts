489197b685f1b3b9b0976933d63061c3292492bb
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-04-01 16:05:29 +0700

    Create Post Timeline

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,30 @@
+import clerkClient, { type User } from "@clerk/clerk-sdk-node";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-} from "~/server/api/trpc";
+const filterUserForClient = (user: User) => ({
+  id: user.id,
+  username: user.emailAddresses[0]?.emailAddress,
+  profileImageUrl: user.profileImageUrl,
+})
 
-export const postRouter = createTRPCRouter({ getAll: publicProcedure.query(({ ctx }) => ctx.prisma.post.findMany()) });
+export const postRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author) {
+        throw new Error("Author not found");
+      }
+      return ({
+        post,
+        author
+      })
+    })
+
+  }),
+});

af1a38f66aa04989a9b63fdd2652980174d0558d
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-04-15 16:50:12 +0700

    Refactor View Analysis page

--- src/components/Analysis/index.tsx
+++ src/components/Analysis/index.tsx
@@ -4,11 +4,10 @@ import toast from "react-hot-toast";
 import { ref } from "firebase/storage";
 import { storage } from "~/constants/firebase";
 import { getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import { Modal } from "~/components";
 import useModalState from "~/hooks/useModalState";
 import { type GeoJson, type ITurf } from "./types";
-import FeaturePicker from "./components/FeaturePicker";
-import AttributePicker from "./components/AttributePicker";
+import View from "./view";
+
 interface Props {
   data?: GeoJson[];
 }
@@ -106,37 +105,21 @@ const Analysis = (props: Props) => {
   };
 
   return (
-    <div className="h-full w-full p-5">
-      <Modal
-        handleHideModal={handleHideModal}
-        isModalVisible={isModalVisible}
-        callback={handleMutateData}
-        modalName={modalName}
-      >
-        <div>
-          <FeaturePicker selected={selected} data={props.data} setSelected={setSelected} />
-          {modalName === "Weighted Mean Spatial" && <AttributePicker featureProperties={featureProperties} propertiesSelected={propertiesSelected} setPropertiesSelected={setPropertiesSelected} />}
-        </div>
-      </Modal>
-      <div className="h-4/6 rounded-md bg-gray-600 ">
-        <div className="p-2">
-          <div className="cursor-pointer">
-            {AnalysisOptions.map((option) => (
-              <h1
-                key={option.name}
-                className="mb-2 flex items-center rounded-md bg-gray-800 px-2 py-2 text-xs text-slate-200"
-                onClick={() => {
-                  setModalName(option.name);
-                  handleShowModal();
-                }}
-              >
-                {option.name}
-              </h1>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+    <View 
+      AnalysisOptions={AnalysisOptions}
+      data={props.data}
+      featureProperties={featureProperties}
+      handleHideModal={handleHideModal}
+      handleMutateData={handleMutateData}
+      handleShowModal={handleShowModal}
+      isModalVisible={isModalVisible}
+      modalName={modalName}
+      propertiesSelected={propertiesSelected}
+      selected={selected}
+      setModalName={setModalName}
+      setPropertiesSelected={setPropertiesSelected}
+      setSelected={setSelected}
+    />
   );
 };
 
---
+++ src/components/Analysis/view.tsx
@@ -0,0 +1,65 @@
+import { memo } from "react";
+import FeaturePicker from "./components/FeaturePicker";
+import AttributePicker from "./components/AttributePicker";
+import { Modal } from "~/components";
+import { type GeoJson } from "./types";
+import React from "react";
+
+
+interface Props {
+    handleHideModal: () => void
+    isModalVisible: boolean
+    handleMutateData: () => void
+    modalName: string
+    selected: GeoJson | null
+    data: GeoJson[] | undefined
+    setSelected: React.Dispatch<React.SetStateAction<GeoJson | null>>
+    featureProperties: () => string[]
+    propertiesSelected: string
+    setPropertiesSelected: React.Dispatch<React.SetStateAction<string>>
+    AnalysisOptions: { name: string }[]
+    setModalName: React.Dispatch<React.SetStateAction<string>>  
+    handleShowModal: () => void 
+}
+
+
+const AnalysisView = (props: Props) => {
+  const {
+    handleHideModal, isModalVisible, handleMutateData, modalName, selected, data, setSelected, featureProperties, propertiesSelected, setPropertiesSelected, AnalysisOptions, setModalName, handleShowModal
+  } = props
+  return (
+    <div className="h-full w-full p-5">
+      <Modal
+        handleHideModal={handleHideModal}
+        isModalVisible={isModalVisible}
+        callback={handleMutateData}
+        modalName={modalName}
+      >
+        <div>
+          <FeaturePicker selected={selected} data={data} setSelected={setSelected} />
+          {modalName === "Weighted Mean Spatial" && <AttributePicker featureProperties={featureProperties} propertiesSelected={propertiesSelected} setPropertiesSelected={setPropertiesSelected} />}
+        </div>
+      </Modal>
+      <div className="h-4/6 rounded-md bg-gray-600 ">
+        <div className="p-2">
+          <div className="cursor-pointer">
+            {AnalysisOptions.map((option) => (
+              <h1
+                key={option.name}
+                className="mb-2 flex items-center rounded-md bg-gray-800 px-2 py-2 text-xs text-slate-200"
+                onClick={() => {
+                  setModalName(option.name);
+                  handleShowModal();
+                }}
+              >
+                {option.name}
+              </h1>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default memo(AnalysisView);

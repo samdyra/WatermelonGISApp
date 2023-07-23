a1ca3a68d46d83728667012392c275b71d6cf100
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2023-07-24 00:09:45 +0700

    refactor: form input refactor

--- src/iso_components/Form102/index.tsx
+++ src/iso_components/Form102/index.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import InputSelect from '../Select';
 import { type IHO102, type inputNames } from '~/constants/texts';
-import FormModal from '~/iso_components/FormModal';
-import useModalState from '~/hooks/useModalState';
 import GeotiffInput from './GeotiffInput';
 import { type FormState, type FormatData } from './types';
 import { type Metadata } from './types';
@@ -22,9 +20,6 @@ interface IFormProps {
 const Form: React.FC<IFormProps> = (props) => {
   const { options, setState, inputNames, metadata, setMetaData, FormatData, setFormatData, handleUpload } = props;
 
-  const [isModalVisible, handleShowModal, handleHideModal] = useModalState();
-  const [isModalEnumVisible, handleShowModalEnum, handleHideModalEnum] = useModalState();
-
   const handleInputChangeMeta = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = event.target;
     setMetaData((prevState) => ({
@@ -41,61 +36,32 @@ const Form: React.FC<IFormProps> = (props) => {
     }));
   };
 
-  useEffect(() => {
-    isModalEnumVisible && handleHideModal();
-  }, [isModalEnumVisible]);
-
-  useEffect(() => {
-    isModalVisible && handleHideModalEnum();
-  }, [isModalVisible]);
-
   return (
-    <div>
-      <FormModal handleHideModal={handleHideModal} isModalVisible={isModalVisible} title="Meta Data">
-        {inputNames.map((item) => (
-          <div className="mb-4" key={item.text}>
-            <label className="block text-sm font-bold " htmlFor={item.text}>
-              {item.text}
-            </label>
-            <input
-              type="text"
-              className=" focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-black  shadow focus:outline-none"
-              name={item.key}
-              value={String(metadata[item.key as keyof Metadata])}
-              onChange={handleInputChangeMeta}
-            />
-          </div>
-        ))}
-      </FormModal>
-
-      <FormModal handleHideModal={handleHideModalEnum} isModalVisible={isModalEnumVisible} title="Enum Data">
-        {options.map((item) => (
-          <InputSelect
-            key={item.text}
-            name={item.text}
-            label={item.text}
-            onChange={handleInputChangeEnum}
-            value={String(FormatData[item.text as keyof FormatData])}
-            options={item.value}
+    <div className="overflow-y-scroll">
+      {inputNames.map((item) => (
+        <div className="mb-4" key={item.text}>
+          <label className="block text-sm font-bold " htmlFor={item.text}>
+            {item.text}
+          </label>
+          <input
+            type="text"
+            className=" focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-black  shadow focus:outline-none"
+            name={item.key}
+            value={String(metadata[item.key as keyof Metadata])}
+            onChange={handleInputChangeMeta}
           />
-        ))}
-      </FormModal>
-
-      <button
-        className="mb-3 rounded bg-yellow-700 px-2 py-2 text-xs font-semibold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-blue-800"
-        type="button"
-        onClick={isModalVisible ? handleHideModal : handleShowModal}
-      >
-        Meta Data
-      </button>
-
-      <button
-        className="mb-3 rounded bg-yellow-700 px-2 py-2 text-xs font-semibold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-blue-800"
-        type="button"
-        onClick={isModalEnumVisible ? handleHideModalEnum : handleShowModalEnum}
-      >
-        Enum
-      </button>
+        </div>
+      ))}
+      {options.map((item) => (
+        <InputSelect
+          key={item.text}
+          name={item.text}
+          label={item.text}
+          onChange={handleInputChangeEnum}
+          value={String(FormatData[item.text as keyof FormatData])}
+          options={item.value}
+        />
+      ))}
 
       <GeotiffInput setState={setState} />
       <button type="submit" onClick={handleUpload}>
